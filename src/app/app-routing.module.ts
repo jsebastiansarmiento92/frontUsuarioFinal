0129b3a4f0289816824b57f6b993fcc03d1295a3
jsebastiansarmiento92 <jsebastiansarmiento92@gmail.com>
2020-06-11 14:02:29 -0500

    agregacion inicio al routing app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+//import { AuthGuard } from './shared';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', loadChildren: () => import('./components/inicio/inicio.module').then(m => m.InicioModule)},
+    { path: 'inicio', loadChildren: () => import('./components/inicio/inicio.module').then(m => m.InicioModule) },
+   // { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule) },
+   // { path: 'error', loadChildren: () => import('./server-error/server-error.module').then(m => m.ServerErrorModule) },
+   // { path: 'access-denied', loadChildren: () => import('./access-denied/access-denied.module').then(m => m.AccessDeniedModule) },
+   // { path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) },
+    { path: '**', redirectTo: 'inicio' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/inicio/inicio.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InicioRoutingModule } from './inicio-routing.module';
+import {FormsModule}from '@angular/forms';
+import { InicioComponent } from './inicio.component';
+
+
+@NgModule({
+  imports: [
+      CommonModule,
+      //TranslateModule,
+      InicioRoutingModule,
+      FormsModule
+  ],
+  declarations: [InicioComponent]
+})
+export class InicioModule { }
