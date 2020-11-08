9d1fbc20c7e362cad1413071e17bb71a05e8e759
Snards <brayan.sierra@usantoto.edu.co>
2020-11-08 01:10:41 -0500

    calificacion estrellas y modal de estrellas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { GuardService as guard} from './services/guard.service';
 
 const routes: Routes = [
-    { path: '', loadChildren: () => import('./components/landing/landing.module').then(m => m.LandingModule)},
+    { path: '', loadChildren: () => import('./components/inicio/inicio.module').then(m => m.InicioModule)},
     { path: 'landing', loadChildren: () => import('./components/landing/landing.module').then(m => m.LandingModule) },
     { path: 'inicio', loadChildren: () => import('./components/inicio/inicio.module').then(m => m.InicioModule) },
     { path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)},
@@ -15,7 +15,7 @@ const routes: Routes = [
     canActivate:[guard]},
    // { path: 'access-denied', loadChildren: () => import('./access-denied/access-denied.module').then(m => m.AccessDeniedModule) },
    // { path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) },
-    { path: '**', redirectTo: 'landing' }
+    { path: '**', redirectTo: 'inicio' }
 ];
 
 @NgModule({
