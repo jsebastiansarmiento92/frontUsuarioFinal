5170d6f0b835e6eaf0cb61df77b918b56c2916c9
jsebastiansarmiento92 <jsebastiansarmiento92@gmail.com>
2020-12-11 12:01:43 -0500

    creacion modelo y servicio de promosciones

---
+++ src/app/services/promociones-service/promociones.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Promociones } from 'src/app/models/promociones';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PromocionesService {
+
+  productoURL = 'https://quickdomicilios.herokuapp.com/promociones';
+  constructor(private httpClient: HttpClient) { }
+  
+  listarUsuarioFinal() : Observable<any>{
+    //alert("nombre que se envia es "+ usuario.nombreUsuario)
+    return this.httpClient.get<Promociones[]>(this.productoURL+"/listarUsuarioFinal");
+ }
+}
